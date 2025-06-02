ab6583d679d380e1c036eaedade348254f7b8660
netkas <netkas.nosial@gmail.com>
2025-06-02 18:29:02 -0400

    Add new audit log types for operator actions and attachment uploads

--- src/FederationServer/Classes/Enums/AuditLogType.php
+++ src/FederationServer/Classes/Enums/AuditLogType.php
@@ -5,4 +5,8 @@
     enum AuditLogType : string
     {
         case OTHER = 'OTHER';
+        case OPERATOR_CREATED = 'OPERATOR_CREATED';
+        case OPERATOR_DELETED = 'OPERATOR_DELETED';
+
+        case ATTACHMENT_UPLOADED = 'ATTACHMENT_UPLOADED';
     }
