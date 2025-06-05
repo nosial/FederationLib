cf6b89aeb63b49c50da491b50ba4b79b3510252a
netkas <netkas.nosial@gmail.com>
2025-06-05 15:08:03 -0400

    Add ListEntityBlacklist request handler

---
+++ src/FederationServer/Methods/Entities/ListEntityBlacklist.php
@@ -0,0 +1,68 @@
+<?php
+
+    namespace FederationServer\Methods\Entities;
+
+    use FederationServer\Classes\Configuration;
+    use FederationServer\Classes\Managers\BlacklistManager;
+    use FederationServer\Classes\Managers\EntitiesManager;
+    use FederationServer\Classes\RequestHandler;
+    use FederationServer\Classes\Validate;
+    use FederationServer\Exceptions\DatabaseOperationException;
+    use FederationServer\Exceptions\RequestException;
+    use FederationServer\FederationServer;
+
+    class ListEntityBlacklist extends RequestHandler
+    {
+        /**
+         * @inheritDoc
+         */
+        public static function handleRequest(): void
+        {
+            $authenticatedOperator = FederationServer::getAuthenticatedOperator(false);
+            if(!Configuration::getServerConfiguration()->isBlacklistPublic() && $authenticatedOperator === null)
+            {
+                throw new RequestException('Unauthorized: You must be authenticated to list blacklist records', 401);
+            }
+
+            $limit = (int) (FederationServer::getParameter('limit') ?? Configuration::getServerConfiguration()->getListBlacklistMaxItems());
+            $page = (int) (FederationServer::getParameter('page') ?? 1);
+
+            if($limit < 1 || $limit > Configuration::getServerConfiguration()->getListBlacklistMaxItems())
+            {
+                $limit = Configuration::getServerConfiguration()->getListBlacklistMaxItems();
+            }
+
+            if($page < 1)
+            {
+                $page = 1;
+            }
+
+            if(!preg_match('#^/entities/([a-fA-F0-9\-]{36,})/blacklist$#', FederationServer::getPath(), $matches))
+            {
+                throw new RequestException('Bad Request: Entity UUID is required', 400);
+            }
+
+            $entityUuid = $matches[1];
+            if(!$entityUuid || !Validate::uuid($entityUuid))
+            {
+                throw new RequestException('Bad Request: a valid entity UUID is required', 400);
+            }
+
+            try
+            {
+                if(!EntitiesManager::entityExistsByUuid($entityUuid))
+                {
+                    throw new RequestException('Entity not found', 404);
+                }
+
+                $blacklistRecords = BlacklistManager::getEntriesByEntity($entityUuid, $limit, $page);
+            }
+            catch (DatabaseOperationException $e)
+            {
+                throw new RequestException('Internal Server Error: Unable to retrieve blacklist records from the entity', 500, $e);
+            }
+
+            self::successResponse(array_map(fn($evidence) => $evidence->toArray(), $blacklistRecords));
+        }
+    }
+
