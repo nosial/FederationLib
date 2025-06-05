fbac5fdadbd20260a01fbd36378cfe8a422f33d4
netkas <netkas.nosial@gmail.com>
2025-06-05 15:07:21 -0400

    Fix condition check for public audit logs access method

--- src/FederationServer/Methods/Entities/ListEntityAuditLogs.php
+++ src/FederationServer/Methods/Entities/ListEntityAuditLogs.php
@@ -18,7 +18,7 @@
         public static function handleRequest(): void
         {
             $authenticatedOperator = FederationServer::getAuthenticatedOperator(false);
-            if(!Configuration::getServerConfiguration()->isPublicAuditLogs() && $authenticatedOperator === null)
+            if(!Configuration::getServerConfiguration()->isAuditLogsPublic() && $authenticatedOperator === null)
             {
                 throw new RequestException('Unauthorized: Public audit logs are disabled and no operator is authenticated', 403);
             }
