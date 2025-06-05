810c8ab4e93850fd6da68b1003e81fbe2161d5bb
netkas <netkas.nosial@gmail.com>
2025-06-05 00:38:54 -0400

    Remove unused OperatorManager import from ListEntityAuditLogs.php

--- src/FederationServer/Methods/Entities/ListEntityAuditLogs.php
+++ src/FederationServer/Methods/Entities/ListEntityAuditLogs.php
@@ -5,12 +5,10 @@
     use FederationServer\Classes\Configuration;
     use FederationServer\Classes\Managers\AuditLogManager;
     use FederationServer\Classes\Managers\EntitiesManager;
-    use FederationServer\Classes\Managers\OperatorManager;
     use FederationServer\Classes\RequestHandler;
     use FederationServer\Exceptions\DatabaseOperationException;
     use FederationServer\Exceptions\RequestException;
     use FederationServer\FederationServer;
-    use FederationServer\Objects\PublicAuditRecord;
 
     class ListEntityAuditLogs extends RequestHandler
     {
