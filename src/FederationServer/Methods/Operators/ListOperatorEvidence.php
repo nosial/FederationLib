e4ab4a7c87102065195c1a71d728e8b15723265e
netkas <netkas.nosial@gmail.com>
2025-06-05 15:07:42 -0400

    Fix condition check for public evidence access method

--- src/FederationServer/Methods/Operators/ListOperatorEvidence.php
+++ src/FederationServer/Methods/Operators/ListOperatorEvidence.php
@@ -20,7 +20,7 @@
             $authenticatedOperator = FederationServer::getAuthenticatedOperator(false);
             $listConfidential = false;
 
-            if(!Configuration::getServerConfiguration()->isPublicEvidence() && $authenticatedOperator === null)
+            if(!Configuration::getServerConfiguration()->isEvidencePublic() && $authenticatedOperator === null)
             {
                 throw new RequestException('Unauthorized: You must be authenticated to list evidence', 401);
             }
