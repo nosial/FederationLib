05969cea5121aeb09472abe79db92cea3032f9e1
netkas <netkas.nosial@gmail.com>
2025-06-20 20:57:53 -0400

    Implement command-line argument parsing and help display functionality

--- src/FederationServer/Program.php
+++ src/FederationServer/Program.php
@@ -2,6 +2,10 @@
 
     namespace FederationServer;
 
+    use FederationServer\Classes\CLI\CreateOperator;
+    use FederationServer\Classes\Enums\CliCommands;
+    use OptsLib\Parse;
+
     class Program
     {
         /**
@@ -12,6 +16,63 @@
          */
         public static function main(array $args): int
         {
+            $args = Parse::parseArgument($args);
+            if(isset($args['help']))
+            {
+                if($args['help'] === true)
+                {
+                    return self::displayHelp();
+                }
 
+                $command = CliCommands::tryFrom($args['help']);
+                if($command === null)
+                {
+                    print(sprintf("Unknown command: %s\n", $command));
+                    return 1;
+                }
+
+                print(sprintf("%s - %s\n\n%s\n\n%s", $command->value, $command->getShortHelp(), $command->getHelp(), $command->getExamples()));
+                return 0;
+            }
+
+            if(isset($args[CliCommands::INITIALIZE->value]))
+            {
+                return CliCommands::INITIALIZE->handle($args);
+            }
+            elseif(isset($args[CliCommands::CREATE_OPERATOR->value]))
+            {
+                return CreateOperator::handle($args);
+            }
+
+            return self::displayHelp();
+        }
+
+        /**
+         * Returns the full help menu for the command-line interface
+         *
+         * @return int The exit code, always 0.
+         */
+        private static function displayHelp(): int
+        {
+            print("FederationServer Command-Line Interface\n");
+            print("Usage: federationserver [command] [options]\n\n");
+            print("Available commands:\n");
+            print("  help - Displays help information about a command\n");
+            foreach(CliCommands::cases() as $command)
+            {
+                print(sprintf("  %s - %s\n", $command->value, $command->getShortHelp()));
+            }
+
+            print("\nExample Usage:\n");
+            print("help [command] - Displays help information about the given command\n\n");
+            foreach(CliCommands::cases() as $command)
+            {
+                if($command->getExamples() !== null)
+                {
+                    print(sprintf("%s\n%s\n\n", $command->value, $command->getExamples()));
+                }
+            }
+
+            return 0;
         }
     }
