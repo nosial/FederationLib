7584e58884f83768d13b67519598535c41e7153f
netkas <netkas.nosial@gmail.com>
2025-06-20 20:58:04 -0400

    Add database initialization and table existence check methods

--- src/FederationServer/Classes/DatabaseConnection.php
+++ src/FederationServer/Classes/DatabaseConnection.php
@@ -2,8 +2,11 @@
 
     namespace FederationServer\Classes;
 
+    use FederationServer\Classes\Enums\DatabaseTables;
+    use FederationServer\Exceptions\DatabaseOperationException;
     use PDO;
     use PDOException;
+    use RuntimeException;
 
     class DatabaseConnection
     {
@@ -12,8 +15,7 @@
         /**
          * Get the PDO connection instance. If it does not exist, create it using the configuration.
          *
-         * @return PDO
-         * @throws PDOException
+         * @return PDO Returns the PDO Connection to the Database
          */
         public static function getConnection(): PDO
         {
@@ -33,5 +35,69 @@
             }
 
             return self::$pdo;
+        }
+
+        /**
+         * Initializes/Updates all the database tables required for FederationServer to run
+         *
+         * @return void
+         * @throws DatabaseOperationException Thrown if there was an error during initialization
+         */
+        public static function initializeDatabase(): void
+        {
+            foreach(DatabaseTables::getOrderedTables() as $sql)
+            {
+                // Skip if the table already exists
+                if(self::tableExists($sql->getTableName()))
+                {
+                    continue;
+                }
+
+                Logger::log()->info("Creating table {$sql->getTableName()}");
+                $path = $sql->getPath();
+                if (!file_exists($path))
+                {
+                    throw new RuntimeException("SQL file for table {$sql->name} does not exist at path: {$path}");
+                }
+
+                $sqlContent = @file_get_contents($path);
+                if ($sqlContent === false)
+                {
+                    throw new RuntimeException("Failed to read SQL file for table {$sql->name} at path: {$path}");
+                }
+
+                try
+                {
+                    // Execute the SQL content to create or update the table.
+                    self::getConnection()->exec($sqlContent);
+                    if(!self::tableExists($sql->getTableName()))
+                    {
+                        throw new DatabaseOperationException("Failed to create table {$sql->getTableName()} verify if the SQL in {$sql->getPath()} is valid");
+                    }
+
+                    Logger::log()->info("Database table {$sql->getTableName()} initialized successfully.");
+                }
+                catch (PDOException $e)
+                {
+                    throw new DatabaseOperationException("Failed to execute SQL for table {$sql->name}: " . $e->getMessage());
+                }
+            }
+        }
+
+        /**
+         * Checks if the given table exists in the database
+         *
+         * @param string $table The table to check its existence
+         * @return bool Returns True if the table exists, False otherwise.
+         */
+        private static function tableExists(string $table): bool
+        {
+            $stmt = self::getConnection()->query("SHOW TABLES LIKE '$table'");
+            if($stmt === false || $stmt->rowCount() === 0)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
