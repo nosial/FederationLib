461219653ddc587f8a3f9a9f5b68e3c0e08546f7
netkas <netkas.nosial@gmail.com>
2025-07-02 17:46:27 -0400

    Refactor Dockerfile and entrypoint script for improved configuration and cleanup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,11 +3,6 @@
 # Start the logger so it can capture logs
 python3 /logger.py --port=5131 &
 
-# Fix permissions for the configuration directory
-mkdir -p /etc/config
-chown -R www-data:www-data /etc/config
-chmod -R 777 /etc/config
-
 # Initialize Socialbox
 echo "Initializing FederationServer..."
 /usr/bin/federationserver init --log-level=${LOG_LEVEL-INFO}
