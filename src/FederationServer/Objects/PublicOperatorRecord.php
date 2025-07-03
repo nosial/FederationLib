ba768236ef2a44560de918e696a5da324513f000
netkas <netkas.nosial@gmail.com>
2025-07-03 15:41:16 -0400

    Add management and status properties to PublicOperatorRecord

--- src/FederationServer/Objects/PublicOperatorRecord.php
+++ src/FederationServer/Objects/PublicOperatorRecord.php
@@ -9,6 +9,9 @@
         private string $uuid;
         private string $name;
         private bool $isClient;
+        private bool $manageOperators;
+        private bool $manageBlacklist;
+        private bool $disabled;
         private int $created;
         private int $updated;
 
@@ -19,6 +22,9 @@
                 $this->uuid = (string)$data['uuid'] ?? '';
                 $this->name = (string)$data['name'] ?? '';
                 $this->isClient = (bool)$data['is_client'];
+                $this->manageOperators = (bool)$data['manage_operators'] ?? false;
+                $this->manageBlacklist = (bool)$data['manage_blacklist'] ?? false;
+                $this->disabled = (bool)$data['disabled'] ?? false;
                 $this->created = (int)$data['created'] ?? 0;
                 $this->updated = (int)$data['updated'] ?? 0;
                 return;
@@ -27,6 +33,9 @@
             $this->uuid = $data->getUuid();
             $this->name = $data->getName();
             $this->isClient = $data->isClient();
+            $this->manageOperators = $data->canManageOperators();
+            $this->manageBlacklist = $data->canManageBlacklist();
+            $this->disabled = $data->isDisabled();
             $this->created = $data->getCreated();
             $this->updated = $data->getUpdated();
         }
@@ -44,6 +53,21 @@
         public function isClient(): bool
         {
             return $this->isClient;
+        }
+
+        public function canManageOperators(): bool
+        {
+            return $this->manageOperators;
+        }
+
+        public function canManageBlacklist(): bool
+        {
+            return $this->manageBlacklist;
+        }
+
+        public function isDisabled(): bool
+        {
+            return $this->disabled;
         }
 
         public function getCreated(): int
@@ -65,6 +89,9 @@
                 'uuid' => $this->uuid,
                 'name' => $this->name,
                 'is_client' => $this->isClient,
+                'manage_operators' => $this->manageOperators,
+                'manage_blacklist' => $this->manageBlacklist,
+                'disabled' => $this->disabled,
                 'created' => $this->created,
                 'updated' => $this->updated
             ];
