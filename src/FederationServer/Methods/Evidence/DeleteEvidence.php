8d8e2d114532a3ed3fd8b86d6aab352d0cef5896
netkas <netkas.nosial@gmail.com>
2025-07-29 14:42:53 -0400

    Update namespace for AuditLogType in DeleteEvidence class

--- src/FederationServer/Methods/Evidence/DeleteEvidence.php
+++ src/FederationServer/Methods/Evidence/DeleteEvidence.php
@@ -2,11 +2,11 @@
 
     namespace FederationServer\Methods\Evidence;
 
-    use FederationServer\Classes\Enums\AuditLogType;
     use FederationServer\Classes\Managers\AuditLogManager;
     use FederationServer\Classes\Managers\EvidenceManager;
     use FederationServer\Classes\RequestHandler;
     use FederationServer\Classes\Validate;
+    use FederationServer\Enums\AuditLogType;
     use FederationServer\Exceptions\DatabaseOperationException;
     use FederationServer\Exceptions\RequestException;
     use FederationServer\FederationServer;
