fb89e5d41fbdd66542cc398b61ae6d1c26cb4624
netkas <netkas.nosial@gmail.com>
2025-07-29 14:43:38 -0400

    Change return type of fromArray method to EvidenceRecord

--- src/FederationServer/Objects/EvidenceRecord.php
+++ src/FederationServer/Objects/EvidenceRecord.php
@@ -133,7 +133,7 @@
         /**
          * @inheritDoc
          */
-        public static function fromArray(array $array): SerializableInterface
+        public static function fromArray(array $array): EvidenceRecord
         {
             if(isset($array['created']))
             {
