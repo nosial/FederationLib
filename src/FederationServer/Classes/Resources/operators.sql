66f3a9db540c48b3f2c6c0030c41ca9a7705546e
netkas <netkas.nosial@gmail.com>
2025-07-30 15:44:26 -0400

    Fix typo in comment for disabled column in operators table

--- src/FederationServer/Classes/Resources/operators.sql
+++ src/FederationServer/Classes/Resources/operators.sql
@@ -7,7 +7,7 @@ create table operators
     manage_operators tinyint(1)  default 0                   not null comment 'Default: 0, 1=This operator can manage other operators by creating new ones, deleting existing ones or disabling existing ones, etc. 0=No such permissions are allowed',
     manage_blacklist tinyint     default 0                   not null comment 'Default: 0, 1=This operator can manage the blacklist by adding/removing to the database, 0=No such permissions are allowed',
     is_client        tinyint     default 0                   not null comment 'Default: 0, 1=This operator has access to client methods that allows the client to build the database of known entities and automatically report evidence or manage the database (if permitted to do so), 0=No such permissions are allowed',
-    disabled         tinyint(1)  default 0                   not null comment 'Default: 0, 1=The operator is disabled, 0=The oprator is active',
+    disabled         tinyint(1)  default 0                   not null comment 'Default: 0, 1=The operator is disabled, 0=The operator is active',
     created          timestamp   default current_timestamp() not null comment 'The Timestamp for when this operator record was created',
     updated          timestamp   default current_timestamp() not null comment 'The Timestamp for when this operator record was last updated',
     constraint operators_api_key_uindex
