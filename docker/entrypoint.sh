fff219943e3cb5472169ef27f82279f7fdadc9fc
netkas <netkas.nosial@gmail.com>
2025-08-27 15:06:48 -0400

    Updated entrypoint to use `federationlib` instead of `federationserver`

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ python3 /logger.py --port=5131 &
 
 # Initialize Socialbox
 echo "Initializing FederationServer..."
-/usr/bin/federationserver init --log-level=${LOG_LEVEL-INFO}
+/usr/bin/federationlib init --log-level=${LOG_LEVEL-INFO}
 
 # Run supervisord
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
