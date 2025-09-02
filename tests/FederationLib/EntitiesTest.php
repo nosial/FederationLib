22e36a5d31a92a8ba8a93a1d0f96c64386e422d0
netkas <netkas.nosial@gmail.com>
2025-09-02 15:31:52 -0400

    Remove unused imports and clean up EntitiesTest class

--- tests/FederationLib/EntitiesTest.php
+++ tests/FederationLib/EntitiesTest.php
@@ -2,22 +2,21 @@
 
     namespace FederationLib;
 
-    use FederationLib\Classes\Utilities;
+    use FederationLib\Exceptions\RequestException;
+    use FederationLib\Objects\Entity;
+    use FederationLib\Objects\EntityQueryResult;
+    use FederationLib\Objects\AuditLog;
+    use FederationLib\Objects\BlacklistRecord;
+    use FederationLib\Objects\EvidenceRecord;
     use PHPUnit\Framework\TestCase;
 
     class EntitiesTest extends TestCase
     {
         private FederationClient $client;
-
         protected function setUp(): void
         {
             $this->client = new FederationClient(getenv('SERVER_ENDPOINT'), getenv('SERVER_API_KEY'));
         }
 
 
-        public function testPushEntity()
-        {
-            $this->client->pushEntity('john', 'example.com');
-            $entityHash =
-        }
     }
