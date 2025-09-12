48027d3077fe891221b5fe12dc402f4c4ec2d689
netkas <netkas@nosial.net>
2025-09-12 00:24:01 -0400

    Fix formatting in entities.sql for improved readability

--- src/FederationLib/Classes/Resources/entities.sql
+++ src/FederationLib/Classes/Resources/entities.sql
@@ -3,7 +3,7 @@ create table entities
     uuid    varchar(36) default uuid()              not null comment 'The primary unique index of the entity'
         primary key,
     hash    varchar(64)                             not null comment 'The Unique Hash combination of the entity (sha256 ''id@domain'', or ''id'' if no domain is set)',
-    host  varchar(255)                            not null comment 'The domain',
+    host    varchar(255)                            not null comment 'The domain',
     id      varchar(255)                            null comment 'The Unique Identifier of the entity',
     created timestamp   default current_timestamp() not null comment 'The Timestamp for when this entity was created',
     constraint entities_hash_uindex
