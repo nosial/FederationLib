0a584479f5d7414818202530666e8e6b45552793
netkas <netkas@nosial.net>
2025-09-12 00:24:07 -0400

    Update evidence.sql to change text_content type to mediumtext and remove comments from index definitions

--- src/FederationLib/Classes/Resources/evidence.sql
+++ src/FederationLib/Classes/Resources/evidence.sql
@@ -5,12 +5,12 @@ create table evidence
     entity       varchar(36)                             not null comment 'The UUID of the entity that this evidence is related to',
     operator     varchar(36)                             not null comment 'The operator that submitted the evidence',
     confidential tinyint(1)  default 0                   not null comment 'Default: 0, 1=The evidence and all of it''s attachments is confidential and only operators can view this, 0=The evidence is available for public view',
-    text_content text                                    null comment 'Optional. the text content with the evidence',
+    text_content mediumtext                              null comment 'Optional. the text content with the evidence',
     tag          varchar(32)                             null comment 'Optional. Abstract tag name related to the evidence',
     note         text                                    null comment 'Optional note by the operator that submitted the evidence',
     created      timestamp   default current_timestamp() not null comment 'The timestamp of the evidence',
     constraint evidence_uuid_uindex
-        unique (uuid) comment 'The Unique Universal Identifier index for the evidence',
+        unique (uuid),
     constraint evidence_entities_uuid_fk
         foreign key (entity) references entities (uuid)
             on update cascade on delete cascade,
@@ -21,14 +21,11 @@ create table evidence
     comment 'Table for housing evidence';
 
 create index evidence_entity_index
-    on evidence (entity)
-    comment 'The index of the entity UUID';
+    on evidence (entity);
 
 create index evidence_operator_index
-    on evidence (operator)
-    comment 'The index of the operator that submitted the evidence';
+    on evidence (operator);
 
 create index evidence_tag_index
-    on evidence (tag)
-    comment 'The index for evidence tags for quickly group evidence records together';
+    on evidence (tag);
 
