c9b1152ff70a4ec25ea0f575bf92569f837ecfa8
netkas <netkas.nosial@gmail.com>
2026-05-30 18:20:07 -0400

    Optimized file_attachments.sql by removing redundant unique index on uuid

--- src/FederationLib/Classes/Resources/file_attachments.sql
+++ src/FederationLib/Classes/Resources/file_attachments.sql
@@ -7,8 +7,6 @@ create table file_attachments
     file_mime varchar(32)                             null comment 'Optional. The MIME type of the file attachment',
     file_size bigint                                  not null comment 'The size of the file',
     created   timestamp   default current_timestamp() not null comment 'The Timestamp for when this file attachment was created',
-    constraint file_attachments_uuid_uindex
-        unique (uuid) comment 'The Unique Universal Identifier Unique Index',
     constraint file_attachments_evidence_uuid_fk
         foreign key (evidence) references evidence (uuid)
             on update cascade on delete cascade
