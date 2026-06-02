857481c088eea9b0940b37b3aa7c571870829b6b
netkas <netkas.nosial@gmail.com>
2026-06-02 04:36:39 -0400

    Prevent operations on reserved 'root' operator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,11 +5,19 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.0.4] - Ongoing
+## [0.0.4] - 2026-06-02
 
+This update introduces several improvements and bug fixes.
+
+### Added
+ - Added a check & fix stage for the initialization command to ensure the root operator is configured correctly
+
+### Changed
+ - Prevent operations on reserved 'root' operator
 
 ### Removed
  - Removed deprecated use of finfo_close
+
 
 
 ## [0.0.3] - 2026-06-01
@@ -22,7 +30,6 @@ Removed all deprecated usage of curl_close
 This update introduces a bug fix
 
 ### Fixed
- - Prevent operations on reserved 'root' operator
  - Validate URL scheme and host in constructor of FederationClient
 
 
--- src/FederationLib/Classes/CLI/InitializeCommand.php
+++ src/FederationLib/Classes/CLI/InitializeCommand.php
@@ -4,8 +4,11 @@
 
     use FederationLib\Classes\DatabaseConnection;
     use FederationLib\Classes\Logger;
+    use FederationLib\Classes\Managers\OperatorManager;
+    use FederationLib\Exceptions\CacheOperationException;
     use FederationLib\Exceptions\DatabaseOperationException;
     use FederationLib\Interfaces\CommandLineInterface;
+    use InvalidArgumentException;
 
     class InitializeCommand implements CommandLineInterface
     {
@@ -27,6 +30,52 @@
             }
 
             Logger::log()->info('Database initialized successfully');
+
+            try
+            {
+                $masterOperator = OperatorManager::getMasterOperator();
+                $uuid = $masterOperator->getUuid();
+                $fixed = false;
+
+                if($masterOperator->isDisabled())
+                {
+                    Logger::log()->warning('Root operator was disabled, re-enabling');
+                    OperatorManager::enableOperator($uuid);
+                    $fixed = true;
+                }
+
+                if(!$masterOperator->canManageOperators())
+                {
+                    Logger::log()->warning('Root operator missing manage_operators permission, fixing');
+                    OperatorManager::setManageOperators($uuid, true);
+                    $fixed = true;
+                }
+
+                if(!$masterOperator->canManageBlacklist())
+                {
+                    Logger::log()->warning('Root operator missing manage_blacklist permission, fixing');
+                    OperatorManager::setManageBlacklist($uuid, true);
+                    $fixed = true;
+                }
+
+                if(!$masterOperator->isClient())
+                {
+                    Logger::log()->warning('Root operator missing is_client permission, fixing');
+                    OperatorManager::setClient($uuid, true);
+                    $fixed = true;
+                }
+
+                if($fixed)
+                {
+                    Logger::log()->info('Root operator properties corrected successfully');
+                }
+            }
+            catch (DatabaseOperationException|InvalidArgumentException $e)
+            {
+                Logger::log()->critical('Failed to validate/fix root operator: ' . $e->getMessage(), $e);
+                return 1;
+            }
+
             return 0;
         }
 
