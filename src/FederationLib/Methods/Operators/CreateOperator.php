97d49f7ee46845f74ce3b3e3cb40adaa67b47fe7
netkas <netkas.nosial@gmail.com>
2026-06-29 02:34:16 -0400

    Added RequestSpecification methods to CreateOperator

--- src/FederationLib/Methods/Operators/CreateOperator.php
+++ src/FederationLib/Methods/Operators/CreateOperator.php
@@ -6,13 +6,21 @@
     use FederationLib\Classes\Managers\OperatorManager;
     use FederationLib\Classes\RequestHandler;
     use FederationLib\Enums\AuditLogType;
+    use FederationLib\Enums\HttpResponseCode;
     use FederationLib\Exceptions\DatabaseOperationException;
     use FederationLib\Exceptions\RequestException;
     use FederationLib\FederationServer;
+    use FederationLib\Objects\ErrorResponse;
     use InvalidArgumentException;
+    use FederationLib\Interfaces\RequestSpecificationInterface;
 
-    class CreateOperator extends RequestHandler
+    class CreateOperator extends RequestHandler implements RequestSpecificationInterface
     {
+        private const string ERROR_INSUFFICIENT_PERMISSIONS = 'Insufficient permissions to create operators';
+        private const string ERROR_NAME_REQUIRED = 'Operator name is required';
+        private const string ERROR_NAME_RESERVED = 'Operator name is reserved';
+        private const string ERROR_UNABLE_TO_CREATE = 'Unable to create operator';
+
         /**
          * @inheritDoc
          */
@@ -21,19 +29,19 @@
             $authenticatedOperator = FederationServer::requireAuthenticatedOperator();
 
             // Ensure the authenticated operator has permission to create new operators.
-            if(!$authenticatedOperator->canManageOperators())
+            if(!$authenticatedOperator->hasOperatorPermissions())
             {
-                throw new RequestException('Insufficient permissions to create operators', 403);
+                throw new RequestException(self::ERROR_INSUFFICIENT_PERMISSIONS, HttpResponseCode::FORBIDDEN);
             }
 
             if(!FederationServer::getParameter('name'))
             {
-                throw new RequestException('Operator name is required', 400);
+                throw new RequestException(self::ERROR_NAME_REQUIRED, HttpResponseCode::BAD_REQUEST);
             }
 
             if(strtolower(FederationServer::getParameter('name')) === 'root' || strtolower(FederationServer::getParameter('name')) === 'system')
             {
-                throw new RequestException('Operator name is reserved', 400);
+                throw new RequestException(self::ERROR_NAME_RESERVED, HttpResponseCode::BAD_REQUEST);
             }
 
             try
@@ -47,14 +55,119 @@
             }
             catch(InvalidArgumentException $e)
             {
-                throw new RequestException($e->getMessage(), 400, $e);
+                throw new RequestException($e->getMessage(), HttpResponseCode::BAD_REQUEST, $e);
             }
             catch(DatabaseOperationException $e)
             {
-                throw new RequestException('Unable to create operator', 500, $e);
+                throw new RequestException(self::ERROR_UNABLE_TO_CREATE, HttpResponseCode::INTERNAL_SERVER_ERROR, $e);
             }
 
             // Respond with the UUID of the newly created operator.
-            self::successResponse($operatorUuid, 201);
+            self::successResponse($operatorUuid, HttpResponseCode::CREATED);
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getTags(): array
+        {
+            return ['Operators'];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getSummary(): string
+        {
+            return 'Create an operator';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDescription(): string
+        {
+            return 'Creates a new operator with the given name. Requires operator management permissions.';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getOperationId(): string
+        {
+            return 'createOperator';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getParameters(): array
+        {
+            return [];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getRequestBody(): ?array
+        {
+            return [
+                'required' => true,
+                'content' => [
+                    'application/json' => [
+                        'schema' => [
+                            'type' => 'object',
+                            'properties' => [
+                                'name' => [
+                                    'type' => 'string',
+                                    'description' => 'The name of the new operator',
+                                ],
+                            ],
+                            'required' => ['name'],
+                        ],
+                    ],
+                ],
+            ];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getResponses(): array
+        {
+            return [
+                '201' => [
+                    'description' => 'Operator created successfully',
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['type' => 'string', 'format' => 'uuid', 'description' => 'UUID of the newly created operator'],
+                        ],
+                    ],
+                ],
+                '400' => [
+                    'description' => self::ERROR_NAME_REQUIRED,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+                '403' => [
+                    'description' => self::ERROR_INSUFFICIENT_PERMISSIONS,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+                '500' => [
+                    'description' => self::ERROR_UNABLE_TO_CREATE,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+            ];
         }
     }
