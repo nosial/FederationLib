4fa4378fdeedd3f1962e613d4ae5208116174e79
netkas <netkas.nosial@gmail.com>
2026-06-30 02:59:21 -0400

    Updated Configuration

--- src/FederationLib/Classes/Configuration.php
+++ src/FederationLib/Classes/Configuration.php
@@ -6,13 +6,16 @@
     use FederationLib\Classes\Configuration\DatabaseConfiguration;
     use FederationLib\Classes\Configuration\MaintenanceConfiguration;
     use FederationLib\Classes\Configuration\RedisConfiguration;
+    use FederationLib\Classes\Configuration\ScanningConfiguration;
     use FederationLib\Classes\Configuration\ServerConfiguration;
     use FederationLib\Enums\AuditLogType;
+    use FederationLib\Enums\ScanningRules;
 
     class Configuration
     {
         private static ?\ConfigLib\Configuration $configuration = null;
         private static ?ServerConfiguration $serverConfiguration = null;
+        private static ?ScanningConfiguration $scanningConfiguration = null;
         private static ?BayesianConfiguration $bayesianConfiguration = null;
         private static ?DatabaseConfiguration $databaseConfiguration = null;
         private static ?MaintenanceConfiguration $maintenanceConfiguration = null;
@@ -35,19 +38,48 @@
             self::$configuration->setDefault('server.list_operators_max_items', 100, 'FEDERATION_LIST_OPERATORS_MAX_ITEMS');
             self::$configuration->setDefault('server.list_evidence_max_items', 100, 'FEDERATION_LIST_EVIDENCE_MAX_ITEMS');
             self::$configuration->setDefault('server.list_blacklist_max_items', 100, 'FEDERATION_LIST_BLACKLIST_MAX_ITEMS');
+            self::$configuration->setDefault('server.list_reports_max_items', 100, 'FEDERATION_LIST_REPORTS_MAX_ITEMS');
             self::$configuration->setDefault('server.public_audit_logs', true, 'FEDERATION_PUBLIC_AUDIT_LOGS');
             self::$configuration->setDefault('server.public_audit_entries', array_map(fn($type) => $type->value, AuditLogType::getDefaultPublic()));
             self::$configuration->setDefault('server.public_evidence', true, 'FEDERATION_PUBLIC_EVIDENCE');
             self::$configuration->setDefault('server.public_blacklist', true, 'FEDERATION_PUBLIC_BLACKLIST');
             self::$configuration->setDefault('server.public_entities', true, 'FEDERATION_PUBLIC_ENTITIES');
+            self::$configuration->setDefault('server.public_reports', true, 'FEDERATION_PUBLIC_REPORTS');
             self::$configuration->setDefault('server.public_scan_content', true, 'FEDERATION_PUBLIC_SCAN_CONTENT');
             self::$configuration->setDefault('server.min_blacklist_time', 1800, 'FEDERATION_MIN_BLACKLIST_TIME');
+
+            // Scanning configuration
+            self::$configuration->setDefault('scanning.default_rosl_score', 0.0, 'FEDERATION_SCORING_RISK_DEFAULT');
+            self::$configuration->setDefault('scanning.risk_score_steepness', 0.25, 'FEDERATION_SCANNING_RISK_AUTHOR_WHITELISTEDSCORE_STEEPNESS');
+            self::$configuration->setDefault('scanning.reputation_update_interval', 900, 'FEDERATION_SCANNING_REPUTATION_UPDATE_INTERVAL');
+            self::$configuration->setDefault('scanning.good_reputation_threshold', 50, 'FEDERATION_SCANNING_GOOD_REPUTATION_THRESHOLD');
+            self::$configuration->setDefault('scanning.bad_reputation_threshold', -50, 'FEDERATION_SCANNING_BAD_REPUTATION_THRESHOLD');
+            self::$configuration->setDefault('scanning.author_blacklisted', ScanningRules::AUTHOR_BLACKLISTED->getModifier(), 'FEDERATION_SCANNING_AUTHOR_BLACKLISTED');
+            self::$configuration->setDefault('scanning.author_permanently_blacklisted', ScanningRules::AUTHOR_PERMANENTLY_BLACKLISTED->getModifier(), 'FEDERATION_SCANNING_AUTHOR_PERMANENTLY_BLACKLISTED');
+            self::$configuration->setDefault('scanning.author_whitelisted', ScanningRules::AUTHOR_WHITELISTED->getModifier(), 'FEDERATION_SCANNING_AUTHOR_WHITELISTED');
+            self::$configuration->setDefault('scanning.named_entity_blacklisted', ScanningRules::NAMED_ENTITY_BLACKLISTED->getModifier(), 'FEDERATION_SCANNING_NAMED_ENTITY_BLACKLISTED');
+            self::$configuration->setDefault('scanning.named_entity_permanently_blacklisted', ScanningRules::NAMED_ENTITY_PERMANENTLY_BLACKLISTED->getModifier(), 'FEDERATION_SCANNING_NAMED_ENTITY_PERMANENTLY_BLACKLISTED');
+            self::$configuration->setDefault('scanning.named_entity_whitelisted', ScanningRules::NAMED_ENTITY_WHITELISTED->getModifier(), 'FEDERATION_SCANNING_NAMED_ENTITY_WHITELISTED');
+            self::$configuration->setDefault('scanning.author_good_reputation', ScanningRules::AUTHOR_GOOD_REPUTATION->getModifier(), 'FEDERATION_SCANNING_AUTHOR_GOOD_REPUTATION');
+            self::$configuration->setDefault('scanning.author_bad_reputation', ScanningRules::AUTHOR_BAD_REPUTATION->getModifier(), 'FEDERATION_SCANNING_AUTHOR_BAD_REPUTATION');
+            self::$configuration->setDefault('scanning.named_entity_bad_repuation', ScanningRules::NAMED_ENTITY_BAD_REPUTATION->getModifier(), 'FEDERATION_SCANNING_NAMED_ENTITY_BAD_REPUTATION');
+            self::$configuration->setDefault('scanning.named_entity_good_repuation', ScanningRules::NAMED_ENTITY_GOOD_REPUTATION->getModifier(), 'FEDERATION_SCANNING_NAMED_ENTITY_GOOD_REPUTATION');
+            self::$configuration->setDefault('scanning.classification_normal', ScanningRules::CLASSIFICATION_NORMAL->getModifier(), 'FEDERATION_SCANNING_CLASSIFICATION_NORMAL');
+            self::$configuration->setDefault('scanning.classification_suspicious', ScanningRules::CLASSIFICATION_SUSPICIOUS->getModifier(), 'FEDERATION_SCANNING_CLASSIFICATION_SUSPICIOUS');
+            self::$configuration->setDefault('scanning.classification_malicious', ScanningRules::CLASSIFICATION_MALICIOUS->getModifier(), 'FEDERATION_SCANNING_CLASSIFICATION_MALICIOUS');
+            self::$configuration->setDefault('scanning.auto_report', true, 'FEDERATION_SCANNING_AUTO_REPORT');
+            self::$configuration->setDefault('scanning.auto_report_threshold', 30.00, 'FEDERATION_SCANNING_AUTO_REPORT_THRESHOLD');
+            self::$configuration->setDefault('scanning.reputation_window_duration', 300, 'FEDERATION_SCANNING_REPUTATION_WINDOW_DURATION');
+            self::$configuration->setDefault('scanning.reputation_max_delta', 10, 'FEDERATION_SCANNING_REPUTATION_MAX_DELTA');
+            self::$configuration->setDefault('scanning.reputation_min_delta', -10, 'FEDERATION_SCANNING_REPUTATION_MIN_DELTA');
+            self::$configuration->setDefault('scanning.reputation_scaling_factor', 0.25, 'FEDERATION_SCANNING_REPUTATION_SCALING_FACTOR');
 
             // Bayesian filter configuration
             self::$configuration->setDefault('bayesian.enabled', true, 'FEDERATION_BS_ENABLED');
             self::$configuration->setDefault('bayesian.ssl', false, 'FEDERATION_BS_SSL');
             self::$configuration->setDefault('bayesian.host', '127.0.0.1', 'FEDERATION_BS_HOST');
             self::$configuration->setDefault('bayesian.port', 6380, 'FEDERATION_BS_PORT');
+            self::$configuration->setDefault('bayesian.classify_known_tokens', true, 'FEDERATION_BS_CLASSIFY_KNOWN_TOKENS');
 
             // Maintenance configuration
             self::$configuration->setDefault('maintenance.enabled', true, 'FEDERATION_MAINTENANCE_ENABLED');
@@ -91,7 +123,11 @@
             // Evidence cache
             self::$configuration->setDefault('redis.evidence_cache_enabled', true, 'FEDERATION_EVIDENCE_CACHE_ENABLED');
             self::$configuration->setDefault('redis.evidence_cache_limit', 3000, 'FEDERATION_EVIDENCE_CACHE_LIMIT');
-            self::$configuration->setDefault('redis.evidence_cache_ttl', 600, 'FEDERATION_EVIDENCE_CACHE_TTL');
+            self::$configuration->setDefault('redis.evidence_cache_ttl', 600, 'FEspiciousDERATION_EVIDENCE_CACHE_TTL');
+            // Reports cache
+            self::$configuration->setDefault('redis.report_cache_enabled', true, 'FEDERATION_REPORT_CACHE_ENABLED');
+            self::$configuration->setDefault('redis.report_cache_limit', 1000, 'FEDERATION_REPORT_CACHE_LIMIT');
+            self::$configuration->setDefault('redis.report_cache_ttl', 600, 'FEDERATION_REPORT_CACHE_TTL');
 
             // Only save if the configuration file does not exist or we're in CLI mode
             if(!file_exists(self::$configuration->getPath()) || php_sapi_name() === 'cli')
@@ -101,6 +137,7 @@
 
             // Initialize the configuration classes
             self::$serverConfiguration = new ServerConfiguration(self::$configuration->get('server'));
+            self::$scanningConfiguration = new ScanningConfiguration(self::$configuration->get('scanning'));
             self::$bayesianConfiguration = new BayesianConfiguration(self::$configuration->get('bayesian'));
             self::$databaseConfiguration = new DatabaseConfiguration(self::$configuration->get('database'));
             self::$redisConfiguration = new RedisConfiguration(self::$configuration->get('redis'));
@@ -150,6 +187,21 @@
             }
 
             return self::$serverConfiguration;
+        }
+
+        /**
+         * Get the scanning configuration.
+         *
+         * @return ScanningConfiguration
+         */
+        public static function getScanningConfiguration(): ScanningConfiguration
+        {
+            if(self::$scanningConfiguration === null)
+            {
+                self::initialize();
+            }
+
+            return self::$scanningConfiguration;
         }
 
         /**
