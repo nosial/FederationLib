1da43176741f84263bb8484400731919fc48203e
netkas <netkas.nosial@gmail.com>
2026-06-29 23:27:40 -0400

    Updated BayesianConfiguration

--- src/FederationLib/Classes/Configuration/BayesianConfiguration.php
+++ src/FederationLib/Classes/Configuration/BayesianConfiguration.php
@@ -8,6 +8,7 @@
         private bool $ssl;
         private string $host;
         private int $port;
+        private bool $classifyKnownTokens;
 
         /**
          * Public Constructor
@@ -20,6 +21,7 @@
             $this->ssl = $configuration['ssl'] ?? false;
             $this->host = $configuration['host'] ?? '127.0.0.1';
             $this->port = $configuration['port'] ?? 6380;
+            $this->classifyKnownTokens = $configuration['classify_known_tokens'] ?? true;
         }
 
         /**
@@ -60,5 +62,15 @@
         public function getPort(): int
         {
             return $this->port;
+        }
+
+        /**
+         * Returns True if classifications should be skipped if the majority of the detected tokens are unknown
+         *
+         * @return bool True to only classify known tokens, False otherwise.
+         */
+        public function classifyKnownTokens(): bool
+        {
+            return $this->classifyKnownTokens;
         }
     }
