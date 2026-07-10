244714374685951baee588b18959ebf9e4ed573d
netkas <netkas.nosial@gmail.com>
2026-07-10 02:52:47 -0400

    Improved parameter validation & Added RequestSpecification methods to ListOperators

--- src/FederationLib/Methods/Operators/ListOperators.php
+++ src/FederationLib/Methods/Operators/ListOperators.php
@@ -6,21 +6,28 @@
     use FederationLib\Classes\Logger;
     use FederationLib\Classes\Managers\OperatorManager;
     use FederationLib\Classes\RequestHandler;
+    use FederationLib\Enums\HttpResponseCode;
     use FederationLib\Exceptions\DatabaseOperationException;
     use FederationLib\Exceptions\RequestException;
     use FederationLib\FederationServer;
+    use FederationLib\Interfaces\RequestSpecificationInterface;
+    use FederationLib\Objects\ErrorResponse;
+    use FederationLib\Objects\OperatorRecord;
 
-    class ListOperators extends RequestHandler
+    class ListOperators extends RequestHandler implements RequestSpecificationInterface
     {
+        private const string ERROR_INSUFFICIENT_PERMISSIONS = 'Insufficient permissions to list operators';
+        private const string ERROR_UNABLE_TO_RETRIEVE = 'Unable to retrieve operators';
+
         /**
          * @inheritDoc
          */
         public static function handleRequest(): void
         {
             $authenticatedOperator = FederationServer::requireAuthenticatedOperator();
-            if(!$authenticatedOperator->canManageOperators())
+            if(!$authenticatedOperator->hasOperatorPermissions())
             {
-                throw new RequestException('Insufficient permissions to list operators', 403);
+                throw new RequestException(self::ERROR_INSUFFICIENT_PERMISSIONS, HttpResponseCode::FORBIDDEN);
             }
 
             $limit = (int) (FederationServer::getParameter('limit') ?? Configuration::getServerConfiguration()->getListOperatorsMaxItems());
@@ -43,10 +50,117 @@
             }
             catch (DatabaseOperationException $e)
             {
-                throw new RequestException('Unable to retrieve operators', 500, $e);
+                throw new RequestException(self::ERROR_UNABLE_TO_RETRIEVE, HttpResponseCode::INTERNAL_SERVER_ERROR, $e);
             }
+
+            $authenticatedUuid = $authenticatedOperator->getUuid();
+            array_walk($operators, function($op) use ($authenticatedUuid): void
+            {
+                if ($op->getUuid() !== $authenticatedUuid)
+                {
+                    $op->clearAccessToken();
+                }
+            });
 
             self::successResponse(array_map(fn($op) => $op->toArray(), $operators));
         }
-    }
 
+        /**
+         * @inheritDoc
+         */
+        public static function getTags(): array
+        {
+            return ['Operators'];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getSummary(): string
+        {
+            return 'List all operators';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDescription(): string
+        {
+            return 'Retrieves a paginated list of all operators. Requires operator management permissions.';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getOperationId(): string
+        {
+            return 'listOperators';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getParameters(): array
+        {
+            return [
+                [
+                    'name' => 'limit',
+                    'in' => 'query',
+                    'description' => 'Maximum number of operators to return',
+                    'required' => false,
+                    'schema' => ['type' => 'integer', 'minimum' => 1],
+                ],
+                [
+                    'name' => 'page',
+                    'in' => 'query',
+                    'description' => 'Page number for pagination',
+                    'required' => false,
+                    'schema' => ['type' => 'integer', 'minimum' => 1],
+                ],
+            ];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getRequestBody(): ?array
+        {
+            return null;
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getResponses(): array
+        {
+            return [
+                '200' => [
+                    'description' => 'List of operators',
+                    'content' => [
+                        'application/json' => [
+                            'schema' => [
+                                'type' => 'array',
+                                'items' => ['$ref' => OperatorRecord::getReference()],
+                            ],
+                        ],
+                    ],
+                ],
+                '403' => [
+                    'description' => self::ERROR_INSUFFICIENT_PERMISSIONS,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+                '500' => [
+                    'description' => self::ERROR_UNABLE_TO_RETRIEVE,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+            ];
+        }
+    }
