ca72e987156cef115ac37c1c81165ed0a7047349
netkas <netkas.nosial@gmail.com>
2026-07-10 02:56:37 -0400

    Updated main entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
-
 echo "Initializing FederationLib"
-federationlib init
-
+/usr/local/bin/federationlib init
 echo "Starting services with supervisord..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
