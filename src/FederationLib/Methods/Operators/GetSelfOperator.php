50e18fbab47d4047947bbe613c92f686485a3bf9
netkas <netkas.nosial@gmail.com>
2026-07-10 02:50:31 -0400

    Improved parameter validation & Added RequestSpecification methods to GetSelfOperator

--- src/FederationLib/Methods/Operators/GetSelfOperator.php
+++ src/FederationLib/Methods/Operators/GetSelfOperator.php
@@ -4,8 +4,10 @@
 
     use FederationLib\Classes\RequestHandler;
     use FederationLib\FederationServer;
+    use FederationLib\Interfaces\RequestSpecificationInterface;
+    use FederationLib\Objects\OperatorRecord;
 
-    class GetSelfOperator extends RequestHandler
+    class GetSelfOperator extends RequestHandler implements RequestSpecificationInterface
     {
         /**
          * @inheritDoc
@@ -13,5 +15,70 @@
         public static function handleRequest(): void
         {
             self::successResponse(FederationServer::requireAuthenticatedOperator()->toArray());
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getTags(): array
+        {
+            return ['Operators'];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getSummary(): string
+        {
+            return 'Get the authenticated operator';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDescription(): string
+        {
+            return 'Retrieves details of the currently authenticated operator.';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getOperationId(): string
+        {
+            return 'getSelfOperator';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getParameters(): array
+        {
+            return [];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getRequestBody(): ?array
+        {
+            return null;
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getResponses(): array
+        {
+            return [
+                '200' => [
+                    'description' => 'Authenticated operator details',
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => OperatorRecord::getReference()],
+                        ],
+                    ],
+                ],
+            ];
         }
     }
