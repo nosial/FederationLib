4b94165eee74718742f2891a540d396fbcdb0922
netkas <netkas.nosial@gmail.com>
2026-07-10 02:50:20 -0400

    Improved parameter validation & Added RequestSpecification methods to GetOperator

--- src/FederationLib/Methods/Operators/GetOperator.php
+++ src/FederationLib/Methods/Operators/GetOperator.php
@@ -5,12 +5,21 @@
     use FederationLib\Classes\Managers\OperatorManager;
     use FederationLib\Classes\RequestHandler;
     use FederationLib\Classes\Validate;
+    use FederationLib\Enums\HttpResponseCode;
     use FederationLib\Exceptions\DatabaseOperationException;
     use FederationLib\Exceptions\RequestException;
     use FederationLib\FederationServer;
+    use FederationLib\Interfaces\RequestSpecificationInterface;
+    use FederationLib\Objects\ErrorResponse;
+    use FederationLib\Objects\OperatorRecord;
 
-    class GetOperator extends RequestHandler
+    class GetOperator extends RequestHandler implements RequestSpecificationInterface
     {
+        private const string ERROR_UUID_REQUIRED = 'Operator UUID is required';
+        private const string ERROR_INVALID_UUID = 'Invalid operator UUID';
+        private const string ERROR_NOT_FOUND = 'Operator not found';
+        private const string ERROR_UNABLE_TO_GET = 'Unable to get operator';
+
         /**
          * @inheritDoc
          */
@@ -19,13 +28,13 @@
             $authenticatedOperator = FederationServer::getAuthenticatedOperator();
             if(!preg_match('#^/operators/([a-fA-F0-9\-]{36})$#', FederationServer::getPath(), $matches))
             {
-                throw new RequestException('Operator UUID required', 405);
+                throw new RequestException(self::ERROR_UUID_REQUIRED, HttpResponseCode::BAD_REQUEST);
             }
 
             $operatorUuid = $matches[1];
             if(!$operatorUuid || !Validate::uuid($operatorUuid))
             {
-                throw new RequestException('Invalid operator UUID', 400);
+                throw new RequestException(self::ERROR_INVALID_UUID, HttpResponseCode::BAD_REQUEST);
             }
 
             try
@@ -33,20 +42,117 @@
                 $existingOperator = OperatorManager::getOperator($operatorUuid);
                 if($existingOperator === null)
                 {
-                    throw new RequestException('Operator Not Found', 404);
+                    throw new RequestException(self::ERROR_NOT_FOUND, HttpResponseCode::NOT_FOUND);
                 }
             }
             catch(DatabaseOperationException $e)
             {
-                throw new RequestException('Unable to get operator', 500, $e);
+                throw new RequestException(self::ERROR_UNABLE_TO_GET, HttpResponseCode::INTERNAL_SERVER_ERROR, $e);
             }
 
-            if(!$authenticatedOperator?->canManageOperators())
+            if(!$authenticatedOperator?->hasOperatorPermissions())
             {
                 // Clear Access Token if the authenticated operator does not have permission to manage operators
                 $existingOperator->clearAccessToken();
             }
 
             self::successResponse($existingOperator->toArray());
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getTags(): array
+        {
+            return ['Operators'];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getSummary(): string
+        {
+            return 'Get an operator by UUID';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDescription(): string
+        {
+            return 'Retrieves details of an operator by UUID. If the authenticated operator does not have operator management permissions, the access token is cleared from the response.';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getOperationId(): string
+        {
+            return 'getOperator';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getParameters(): array
+        {
+            return [
+                [
+                    'name' => 'uuid',
+                    'in' => 'path',
+                    'description' => self::ERROR_UUID_REQUIRED,
+                    'required' => true,
+                    'schema' => ['type' => 'string', 'format' => 'uuid'],
+                ],
+            ];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getRequestBody(): ?array
+        {
+            return null;
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getResponses(): array
+        {
+            return [
+                '200' => [
+                    'description' => 'Operator details',
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => OperatorRecord::getReference()],
+                        ],
+                    ],
+                ],
+                '400' => [
+                    'description' => self::ERROR_INVALID_UUID,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+                '404' => [
+                    'description' => self::ERROR_NOT_FOUND,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+                '500' => [
+                    'description' => self::ERROR_UNABLE_TO_GET,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+            ];
         }
     }
