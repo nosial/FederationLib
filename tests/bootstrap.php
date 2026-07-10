216a3fa5da98e089883d93a5b7ec7d9a953e3be6
netkas <netkas.nosial@gmail.com>
2026-07-10 03:04:05 -0400

    Updated phpunit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,22 +1,25 @@
-<?PHP
-        require 'ncc';
+<?php
 
-        $buildOutputPath = __DIR__ . DIRECTORY_SEPARATOR . '../target/release/net.nosial.federation.ncc';
-        if(getenv('NCC_BUILD_OUTPUT_PATH'))
-        {
-            $buildOutputPath = getenv('NCC_BUILD_OUTPUT_PATH');
-        }
+    require 'ncc';
 
-        if(!file_exists($buildOutputPath))
-        {
-            throw new Exception('Build output not found: ' . $buildOutputPath);
-        }
+    $buildOutputPath = __DIR__ . DIRECTORY_SEPARATOR . '../target/release/net.nosial.federation.ncc';
+    if (getenv('NCC_BUILD_OUTPUT_PATH'))
+    {
+        $buildOutputPath = getenv('NCC_BUILD_OUTPUT_PATH');
+    }
 
-        // Import the build output
-        import($buildOutputPath);
+    if (!file_exists($buildOutputPath))
+    {
+        throw new Exception('Build output not found: ' . $buildOutputPath);
+    }
 
-        // Import helper classes
-        require __DIR__ . DIRECTORY_SEPARATOR . 'FederationLib' . DIRECTORY_SEPARATOR . 'Helpers' . DIRECTORY_SEPARATOR . 'Logger.php';
+    // Import the build output
+    import($buildOutputPath);
 
-        // LogLib2's handlers may interfere with tests, so we unregister them here.
-        \LogLib2\Logger::unregisterHandlers();
+    // Import helper classes
+    require __DIR__ . DIRECTORY_SEPARATOR . 'FederationLib' . DIRECTORY_SEPARATOR . 'Helpers' . DIRECTORY_SEPARATOR . 'ClassificationTextGenerator.php';
+    require __DIR__ . DIRECTORY_SEPARATOR . 'FederationLib' . DIRECTORY_SEPARATOR . 'Helpers' . DIRECTORY_SEPARATOR . 'Logger.php';
+    require __DIR__ . DIRECTORY_SEPARATOR . 'FederationLib' . DIRECTORY_SEPARATOR . 'Helpers' . DIRECTORY_SEPARATOR . 'SecurityTestHelpers.php';
+
+    // LogLib2's handlers may interfere with tests, so we unregister them here.
+    \LogLib2\Logger::unregisterHandlers();
