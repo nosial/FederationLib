612b137be08eab38b1a807c5e05ceed914873868
netkas <netkas.nosial@gmail.com>
2026-07-10 01:28:08 -0400

    Improved parameter validation & Added RequestSpecification methods to GetEntityRecord

--- src/FederationLib/Methods/Entities/GetEntityRecord.php
+++ src/FederationLib/Methods/Entities/GetEntityRecord.php
@@ -9,9 +9,18 @@
     use FederationLib\Exceptions\DatabaseOperationException;
     use FederationLib\Exceptions\RequestException;
     use FederationLib\FederationServer;
+    use FederationLib\Interfaces\RequestSpecificationInterface;
+    use FederationLib\Objects\EntityRecord;
+    use FederationLib\Objects\ErrorResponse;
 
-    class GetEntityRecord extends RequestHandler
+    class GetEntityRecord extends RequestHandler implements RequestSpecificationInterface
     {
+        private const string ERROR_AUTHENTICATION_REQUIRED = 'You must be authenticated to view entity records';
+        private const string ERROR_IDENTIFIER_REQUIRED = 'Entity Identifier SHA-256/UUID is required';
+        private const string ERROR_INVALID_IDENTIFIER = 'Given identifier is not a valid UUID, SHA-256, or entity address input';
+        private const string ERROR_UNABLE_TO_RETRIEVE = 'Unable to retrieve entity';
+        private const string ERROR_NOT_FOUND = 'Entity not found';
+
         /**
          * @inheritDoc
          */
@@ -20,21 +29,22 @@
             $authenticatedOperator = FederationServer::getAuthenticatedOperator();
             if(!Configuration::getServerConfiguration()->isEntitiesPublic() && $authenticatedOperator === null)
             {
-                throw new RequestException('You must be authenticated to view entity records', 401);
+                throw new RequestException(self::ERROR_AUTHENTICATION_REQUIRED, 401);
             }
 
             if(
                 !preg_match('#^/entities/([a-fA-F0-9\-]{36})$#', FederationServer::getPath(), $matches) &&
-                !preg_match('#^/entities/([a-f0-9\-]{64})$#', FederationServer::getPath(), $matches)
+                !preg_match('#^/entities/([a-f0-9\-]{64})$#', FederationServer::getPath(), $matches) &&
+                !preg_match('#^/entities/([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$#', FederationServer::getPath(), $matches)
             )
             {
-                throw new RequestException('Entity identifier is required', 400);
+                throw new RequestException(self::ERROR_IDENTIFIER_REQUIRED, 400);
             }
 
             $entityIdentifier = $matches[1];
             if(!$entityIdentifier)
             {
-                throw new RequestException('Entity Identifier SHA-256/UUID is required', 400);
+                throw new RequestException(self::ERROR_IDENTIFIER_REQUIRED, 400);
             }
 
             try
@@ -47,22 +57,131 @@
                 {
                     $entityRecord = EntitiesManager::getEntityByHash($entityIdentifier);
                 }
+                elseif(Utilities::isEntityAddress($entityIdentifier))
+                {
+                    $parsedAddress = Utilities::parseEntityAddress($entityIdentifier);
+                    $entityRecord = EntitiesManager::getEntityByHash(Utilities::hashEntity($parsedAddress['host'], $parsedAddress['id']));
+                }
                 else
                 {
-                    throw new RequestException('Given identifier is not a valid UUID or SHA-256 input', 400);
+                    throw new RequestException(self::ERROR_INVALID_IDENTIFIER, 400);
                 }
             }
             catch (DatabaseOperationException $e)
             {
-                throw new RequestException('Unable to retrieve entity', 500, $e);
+                throw new RequestException(self::ERROR_UNABLE_TO_RETRIEVE, 500, $e);
             }
 
             if($entityRecord === null)
             {
-                throw new RequestException('Entity not found', 404);
+                throw new RequestException(self::ERROR_NOT_FOUND, 404);
             }
 
             self::successResponse($entityRecord->toArray());
         }
-    }
 
+        /**
+         * @inheritDoc
+         */
+        public static function getTags(): array
+        {
+            return ['Entities'];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getSummary(): string
+        {
+            return 'Get an entity record';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDescription(): string
+        {
+            return 'Retrieves a single entity record by UUID, SHA-256 hash, or entity address. If entities are public, authentication is optional; otherwise, an operator must be authenticated.';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getOperationId(): string
+        {
+            return 'getEntityRecord';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getParameters(): array
+        {
+            return [
+                [
+                    'name' => 'identifier',
+                    'in' => 'path',
+                    'description' => 'UUID, SHA-256 hash, or entity address of the entity',
+                    'required' => true,
+                    'schema' => ['type' => 'string'],
+                ],
+            ];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getRequestBody(): ?array
+        {
+            return null;
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getResponses(): array
+        {
+            return [
+                '200' => [
+                    'description' => 'Entity record retrieved successfully',
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => EntityRecord::getReference()],
+                        ],
+                    ],
+                ],
+                '400' => [
+                    'description' => self::ERROR_INVALID_IDENTIFIER,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+                '401' => [
+                    'description' => self::ERROR_AUTHENTICATION_REQUIRED,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+                '404' => [
+                    'description' => self::ERROR_NOT_FOUND,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+                '500' => [
+                    'description' => self::ERROR_UNABLE_TO_RETRIEVE,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+            ];
+        }
+    }
