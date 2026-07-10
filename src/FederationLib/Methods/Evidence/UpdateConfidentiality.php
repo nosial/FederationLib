20efa9945180f909197bf8f2675764bb6350c0dd
netkas <netkas.nosial@gmail.com>
2026-07-10 02:46:37 -0400

    Improved parameter validation & Added RequestSpecification methods to UpdateConfidentiality

--- src/FederationLib/Methods/Evidence/UpdateConfidentiality.php
+++ src/FederationLib/Methods/Evidence/UpdateConfidentiality.php
@@ -8,48 +8,178 @@
     use FederationLib\Exceptions\DatabaseOperationException;
     use FederationLib\Exceptions\RequestException;
     use FederationLib\FederationServer;
+    use FederationLib\Interfaces\RequestSpecificationInterface;
+    use FederationLib\Objects\ErrorResponse;
+    use FederationLib\Objects\SuccessResponse;
 
-    class UpdateConfidentiality extends RequestHandler
+    class UpdateConfidentiality extends RequestHandler implements RequestSpecificationInterface
     {
+        private const string ERROR_INSUFFICIENT_PERMISSIONS = 'You do not have permission to update confidentiality settings';
+        private const string ERROR_UUID_REQUIRED = 'Evidence UUID is required';
+        private const string ERROR_INVALID_UUID = 'Invalid evidence UUID';
+        private const string ERROR_NOT_FOUND = 'Evidence not found';
+        private const string ERROR_UNABLE_TO_UPDATE = 'Unable to update evidence confidentiality';
+
         /**
          * @inheritDoc
          */
         public static function handleRequest(): void
         {
             $authenticatedOperator = FederationServer::requireAuthenticatedOperator();
-            if(!$authenticatedOperator->canManageBlacklist())
+            if(!$authenticatedOperator->hasManagementPermissions())
             {
-                throw new RequestException('You do not have permission to update confidentiality settings', 403);
+                throw new RequestException(self::ERROR_INSUFFICIENT_PERMISSIONS, 403);
             }
 
             if(!preg_match('#^/evidence/([a-fA-F0-9\-]{36})/update_confidentiality$#', FederationServer::getPath(), $matches))
             {
-                throw new RequestException('Evidence UUID required', 405);
+                throw new RequestException(self::ERROR_UUID_REQUIRED, 400);
             }
 
             $evidenceUuid = $matches[1];
             if(!$evidenceUuid || !Validate::uuid($evidenceUuid))
             {
-                throw new RequestException('Invalid evidence UUID', 400);
+                throw new RequestException(self::ERROR_INVALID_UUID, 400);
             }
 
-            $confidential = (bool)FederationServer::getParameter('confidential') ?? false;
+            $confidential = filter_var(FederationServer::getParameter('confidential') ?? false, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) ?? false;
 
             try
             {
                 $evidenceRecord = EvidenceManager::getEvidence($evidenceUuid);
                 if($evidenceRecord === null)
                 {
-                    throw new RequestException('Evidence Not Found', 404);
+                    throw new RequestException(self::ERROR_NOT_FOUND, 404);
                 }
 
                 EvidenceManager::updateConfidentiality($evidenceUuid, $confidential);
             }
             catch(DatabaseOperationException $e)
             {
-                throw new RequestException('Unable to get evidence', 500, $e);
+                throw new RequestException(self::ERROR_UNABLE_TO_UPDATE, 500, $e);
             }
 
             self::successResponse();
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getTags(): array
+        {
+            return ['Evidence'];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getSummary(): string
+        {
+            return 'Update evidence confidentiality';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDescription(): string
+        {
+            return 'Updates the confidentiality flag of an evidence record. Requires management permissions.';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getOperationId(): string
+        {
+            return 'updateEvidenceConfidentiality';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getParameters(): array
+        {
+            return [
+                [
+                    'name' => 'uuid',
+                    'in' => 'path',
+                    'required' => true,
+                    'schema' => ['type' => 'string', 'format' => 'uuid'],
+                    'description' => 'UUID of the evidence record to update',
+                ],
+            ];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getRequestBody(): ?array
+        {
+            return [
+                'required' => true,
+                'content' => [
+                    'application/json' => [
+                        'schema' => [
+                            'type' => 'object',
+                            'properties' => [
+                                'confidential' => [
+                                    'type' => 'boolean',
+                                    'description' => 'Whether the evidence should be confidential',
+                                ],
+                            ],
+                            'required' => ['confidential'],
+                        ],
+                    ],
+                ],
+            ];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getResponses(): array
+        {
+            return [
+                '200' => [
+                    'description' => 'Confidentiality updated successfully',
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => SuccessResponse::getReference()],
+                        ],
+                    ],
+                ],
+                '400' => [
+                    'description' => self::ERROR_INVALID_UUID,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+                '403' => [
+                    'description' => self::ERROR_INSUFFICIENT_PERMISSIONS,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+                '404' => [
+                    'description' => self::ERROR_NOT_FOUND,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+                '500' => [
+                    'description' => self::ERROR_UNABLE_TO_UPDATE,
+                    'content' => [
+                        'application/json' => [
+                            'schema' => ['$ref' => ErrorResponse::getReference()],
+                        ],
+                    ],
+                ],
+            ];
         }
     }
