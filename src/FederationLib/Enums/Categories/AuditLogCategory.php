163c98ad613f9c98d6212ec831f40c9ee61dbef2
netkas <netkas.nosial@gmail.com>
2026-07-19 17:25:26 -0400

    Added AuditLogCategory

---
+++ src/FederationLib/Enums/Categories/AuditLogCategory.php
@@ -0,0 +1,48 @@
+<?php
+
+    namespace FederationLib\Enums\Categories;
+
+    use FederationLib\Enums\AuditLogType;
+    use FederationLib\Interfaces\CategorizableDatabaseInterface;
+
+    enum AuditLogCategory : string implements CategorizableDatabaseInterface
+    {
+        case OPERATOR_EVENTS = 'OPERATOR_EVENTS';
+        case ATTACHMENT_EVENTS = 'ATTACHMENT_EVENTS';
+        case EVIDENCE_EVENTS = 'EVIDENCE_EVENTS';
+        case REPORT_EVENTS = 'REPORT_EVENTS';
+        case ENTITY_EVENTS = 'ENTITY_EVENTS';
+        case BLACKLIST_EVENTS = 'BLACKLIST_EVENTS';
+        case OTHER = 'OTHER';
+
+        /**
+         * @inheritDoc
+         */
+        public function toCondition(): array
+        {
+            $typeValues = [];
+            foreach (AuditLogType::cases() as $type)
+            {
+                if ($type->getCategory() === $this)
+                {
+                    $typeValues[] = $type->value;
+                }
+            }
+
+            if (empty($typeValues))
+            {
+                return ['', []];
+            }
+
+            $placeholders = [];
+            $params = [];
+            foreach ($typeValues as $i => $value)
+            {
+                $key = ":cat_type_$i";
+                $placeholders[] = $key;
+                $params[$key] = $value;
+            }
+
+            return ['type IN (' . implode(', ', $placeholders) . ')', $params];
+        }
+    }
