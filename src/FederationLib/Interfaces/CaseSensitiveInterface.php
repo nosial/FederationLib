9b58131629b5aa245f76f4f2f18cff826b046087
netkas <netkas.nosial@gmail.com>
2026-07-20 02:03:38 -0400

    Added CaseSensitiveInterface

---
+++ src/FederationLib/Interfaces/CaseSensitiveInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+    namespace FederationLib\Interfaces;
+
+    interface CaseSensitiveInterface
+    {
+        /**
+         * Attempts to resolve the enum case from a case-insensitive input
+         *
+         * @param string $value The input, can be case-insensitive
+         * @return static|null The matched enum case, null otherwise.
+         */
+        public static function tryFromCaseInsensitive(string $value): ?static;
+    }
