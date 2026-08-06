c85cedcc9fca677598bd2e1adb497175dd17f940
netkas <netkas.nosial@gmail.com>
2026-08-06 01:28:14 -0400

    Updated docker-entrypoint.sh to include an ASCII banner and enabled CLI logging for the initialization command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
+echo "▗▄▄▄▖▗▄▄▄▖▗▄▄▄ ▗▄▄▄▖▗▄▄▖  ▗▄▖▗▄▄▄▖▗▄▄▄▖ ▗▄▖ ▗▖  ▗▖"
+echo "▐▌   ▐▌   ▐▌  █▐▌   ▐▌ ▐▌▐▌ ▐▌ █    █  ▐▌ ▐▌▐▛▚▖▐▌"
+echo "▐▛▀▀▘▐▛▀▀▘▐▌  █▐▛▀▀▘▐▛▀▚▖▐▛▀▜▌ █    █  ▐▌ ▐▌▐▌ ▝▜▌"
+echo "▐▌   ▐▙▄▄▖▐▙▄▄▀▐▙▄▄▖▐▌ ▐▌▐▌ ▐▌ █  ▗▄█▄▖▝▚▄▞▘▐▌  ▐▌"
+
 echo "Initializing FederationLib"
-/usr/local/bin/federationlib init
+env -u LOGLIB_CONSOLE_ENABLED /usr/local/bin/federationlib init
+
 echo "Starting services with supervisord..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
